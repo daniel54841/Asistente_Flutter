a6026c14bdd8d902e40ec4a33b15ceda92307e2d
Daniel Pastor Sardá <daniel.pastor@getronics.com>
2024-07-12 09:20:01 +0200

    Añadido slidable

--- lib/widgets/item_list_views/item_contacts.dart
+++ lib/widgets/item_list_views/item_contacts.dart
@@ -8,27 +8,29 @@ class ItemContact extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          children: [
-            contact.photo != null
-                ? CircleAvatar(
-                    child: Image.memory(contact.photo!),
-                  )
-                : const CircleAvatar(
-                    backgroundColor: Colors.lightBlue,
-                    child: Icon(
-                      Icons.contact_phone,
-                      color: Colors.deepOrange,
+    return Slidable(
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              contact.photo != null
+                  ? CircleAvatar(
+                      child: Image.memory(contact.photo!),
+                    )
+                  : const CircleAvatar(
+                      backgroundColor: Colors.lightBlue,
+                      child: Icon(
+                        Icons.contact_phone,
+                        color: Colors.deepOrange,
+                      ),
                     ),
-                  ),
-            SizedBox(
-              width: 12,
-            ),
-            Text(contact.displayName),
-          ],
+              const SizedBox(
+                width: 12,
+              ),
+              Text(contact.displayName),
+            ],
+          ),
         ),
       ),
     );
