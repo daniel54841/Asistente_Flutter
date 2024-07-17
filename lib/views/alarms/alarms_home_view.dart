31153867a3549649cdc94d275865b8addc7ec8b5
Daniel Pastor Sardá <daniel.pastor@getronics.com>
2024-07-17 08:53:21 +0200

    Creadas pantallas alertas y reloj
    Primer diseño, solo prueba para navegacion

--- lib/views/alarms/alarms_home_view.dart
+++ lib/views/alarms/alarms_home_view.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:provider/provider.dart';
 
 import '../../controllers/alarms_controller.dart';
 import '../../controllers/voice_controller.dart';
-import '../../net/data/menu_info.dart';
 import '../../utils/responsive.dart';
 import '../../widgets/alarms_and_clock/buildMenuButton.dart';
 import '../../widgets/drawer/drawer_menu.dart';
@@ -33,7 +31,7 @@ class _AlarmsHomeViewState extends State<AlarmsHomeView> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).splashColor,
         title: Text(
-          "Calendario",
+          "Alarmas y Temporizadores",
           style: TextStyle(
             fontSize: responsive.dp(2),
             fontWeight: FontWeight.bold,
@@ -41,36 +39,29 @@ class _AlarmsHomeViewState extends State<AlarmsHomeView> {
           ),
         ),
       ),
-      body: Row(
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: _alarmCtrl.menuItems
-                .map((currentMenuInfo) => BuildMenuButton(
-                      currentMenuInfo: currentMenuInfo,
-                    ))
-                .toList(),
-          ),
-          const VerticalDivider(
-            color: Colors.grey,
-            width: 1,
-          ),
-          Expanded(
-            child: Consumer<MenuInfo>(
-              builder: (BuildContext context, MenuInfo value, Widget? child) {
-                if (value.menuType == MenuType.clock)
-                  return ClockView();
-                else if (value.menuType == MenuType.alarm)
-                  return AlarmPage();
-                else
-                  return Text(
-                    value.title!,
-                    style: TextStyle(fontSize: 20),
-                  );
-              },
-            ),
-          )
-        ],
+      body: GetBuilder(
+        builder: (AlarmsController ctrl) {
+          return Row(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: ctrl.menuItems
+                    .map((currentMenuInfo) => BuildMenuButton(
+                          currentMenuInfo: currentMenuInfo,
+                          alarmCtrl: ctrl,
+                        ))
+                    .toList(),
+              ),
+              const VerticalDivider(
+                color: Colors.grey,
+                width: 1,
+              ),
+              Expanded(
+                child: ctrl.setView(),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
---
+++ lib/views/alarms/clock_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class ClockView extends StatelessWidget {
+  const ClockView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("Clock View"));
+  }
+}
