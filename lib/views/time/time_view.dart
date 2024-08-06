bdae76962fbaa2169f06c984d105bb5fc70f3564
DPastor <daniel.pastor@getronics.com>
2024-08-06 11:50:07 +0200

    Añadido mapa para geolocalización y obtención de datos

--- lib/views/time/time_view.dart
+++ lib/views/time/time_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:location_picker_flutter_map/location_picker_flutter_map.dart';
 import 'package:reproductor_ia/controllers/time_controller.dart';
 import '../../utils/responsive.dart';
 import '../../widgets/drawer/drawer_menu.dart';
@@ -22,15 +23,38 @@ class TimeView extends StatelessWidget {
           title: Text(
             "El Tiempo",
             style: TextStyle(
-              fontSize: responsive.dp(5),
+              fontSize: responsive.dp(2.5),
               fontWeight: FontWeight.bold,
               color: Colors.black,
             ),
           ),
         ),
-        body: Column(
-          children: [Text("Mapa")],
-        ),
+        body: FlutterLocationPicker(
+            initPosition: LatLong(23, 89),
+            selectLocationButtonStyle: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.blue),
+            ),
+            selectedLocationButtonTextstyle: const TextStyle(fontSize: 18),
+            selectLocationButtonText: 'Set Current Location',
+            selectLocationButtonLeadingIcon: const Icon(Icons.check),
+            initZoom: 11,
+            minZoomLevel: 5,
+            maxZoomLevel: 16,
+            trackMyPosition: true,
+            onError: (e) => print(e),
+            onPicked: (pickedData) {
+              print(pickedData.latLong.latitude);
+              print(pickedData.latLong.longitude);
+              print(pickedData.address);
+              print(pickedData.addressData['country']);
+            },
+            onChanged: (pickedData) {
+              print(pickedData.latLong.latitude);
+              print(pickedData.latLong.longitude);
+              print(pickedData.address);
+              print(pickedData.addressData);
+            }),
+
         floatingActionButton: const VoiceFloatingButton());
   }
 }
